d17827dddfd15e9209cf1bcd09d940a9d9e40aba
Shreyam Sharma <rajroy10008@gmail.com>
2025-06-09 22:11:02 +0530

    added the page2 animations

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function page1Animation(){
+    var tl = gsap.timeline()
 
 tl.from("nav h1, nav h4, nav button",{
     y:-20,
@@ -33,3 +34,47 @@ tl.from(".section1bottom img", {
     stagger:0.2,
     duration:0.5
 })
+}
+
+var tl2 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".services h3",
+        scroller:"body",
+        strat:"top 50%",
+        end:"top 10%",
+        scrub:2,
+    }
+})
+
+tl2.from(".services",{
+    y:30,
+    opacity:0,
+    duration:0.5
+})
+
+// group1 animation
+tl2.from(".left1",{
+    x:-300,
+    opacity:0,
+    duration:1,
+    delay:0.5
+}, "anim1")
+tl2.from(".right1",{
+    x:300,
+    opacity:0,
+    duration:1,
+    delay:0.5
+}, "anim1")
+// group2 animation
+tl2.from(".left2",{
+    x:-300,
+    opacity:0,
+    duration:1,
+    delay:0.5
+}, "anim2")
+tl2.from(".right2",{
+    x:300,
+    opacity:0,
+    duration:1,
+    delay:0.5
+}, "anim2")
